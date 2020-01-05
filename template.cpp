3bfb6d490c7e4fbc26e3b8a431f7b9b708c892f8
unknown <ramvard@gmail.com>
2020-01-05 21:43:58 +0530

    feat: create template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define lld long long
+#define pb push_back
+#define mp make_pair
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+
+int main() {
+	fastio;
+
+	lld t, n;
+
+	cin >> t;
+
+	while(t--) {
+		cin >> n;
+		
+	}
+	return 0;
+}
