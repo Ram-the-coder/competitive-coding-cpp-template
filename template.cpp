c02b3807fab92229ecb5941bffa6cadadf0c9915
unknown <ramvard@gmail.com>
2020-03-24 23:58:55 +0530

    refactor: added macros for basic vector i/o

--- template.cpp
+++ template.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 #define lld long long
-#define vll vector<long long>
-#define vvll vector<vector<long long>>
+#define vll vector<lld>
+#define vvll vector<vector<lld>>
 #define pb push_back
 #define mp make_pair
+#define ipv(v) for(lld i=0; i<v.size(); ++i) cin >> v[i]
+#define dispv(v) for(lld i=0; i<v.size();++i) cout << v[i] << " "; cout << '\n'
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
 using namespace std;
 
