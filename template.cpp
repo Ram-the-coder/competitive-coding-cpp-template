d81025a3dca0a4585f8f04fe92eddc811ceb8ebc
unknown <ramvard@gmail.com>
2020-03-24 23:14:21 +0530

    refactor: added macro for commonly used vector definitions

--- template.cpp
+++ template.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 #define lld long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
 #define pb push_back
 #define mp make_pair
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
