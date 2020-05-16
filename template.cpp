798de33414827786c23b1426206008846c1afcea
unknown <ramvard@gmail.com>
2020-05-16 17:33:31 +0530

    Added macro to display array

--- template.cpp
+++ template.cpp
@@ -6,6 +6,7 @@
 #define mp make_pair
 #define ipv(v) for(lld i=0; i<v.size(); ++i) cin >> v[i]
 #define dispv(v) for(lld i=0; i<v.size();++i) cout << v[i] << " "; cout << '\n'
+#define dispa(a, n) for(lld i=0; i<n; ++i) cout << a[i] << " "; cout << '\n'
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
 using namespace std;
 
